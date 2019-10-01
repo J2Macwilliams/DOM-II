e2cf94792a648fc9c36e8eb108e1db6c10828af3
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-01 10:56:11 -0700

    Added 2 event listeners on the logo.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+//1------------------------------------
+const logoSize = document.querySelector(".logo-heading");
+
+logoSize.addEventListener("mouseenter" , () => {
+ logoSize.style.transform = "scale(1.3)";
+ logoSize.style.trasition = "all 0.4s"
+})
+
+logoSize.addEventListener("mouseleave" , () => {
+    logoSize.style.transform = "scale(1)";
+    logoSize.style.trasition = "all 0.4s"
+   })
+
+//2------------------------------------
+
+
